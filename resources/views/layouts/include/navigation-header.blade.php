c0e20d0c2e9edd226aeac5742900bcb34ecacd74
Fernando Vazquez Ibarra <fernandovibz@gmail.com>
2026-03-06 19:12:53 -0600

    Moderniza sidebar y topbar con estilo administrativo actual

--- resources/views/layouts/include/navigation-header.blade.php
+++ resources/views/layouts/include/navigation-header.blade.php
@@ -6,15 +6,20 @@ $empresa = Empresa::first();
 ?>
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-4" href="{{ route('panel') }}">{{$empresa->nombre ?? ''}}</a>
+    <a class="navbar-brand ps-4" href="{{ route('panel') }}">
+        <span class="brand-mark"><i class="fa-solid fa-cube"></i></span>
+        <span class="brand-name">{{ $empresa->nombre ?? 'BlueCrow POS' }}</span>
+    </a>
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-3 topbar-action" id="sidebarToggle" href="#!" aria-label="Mostrar u ocultar menú lateral">
+        <i class="fas fa-bars"></i>
+    </button>
     <!-- Navbar Search-->
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
 
     </form>
-    <div class="nav-item dropdown me-3">
-        <a class="nav-link dropdown-toggle" href="#" role="button" id="notificationsDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+    <div class="nav-item dropdown me-2 me-md-3">
+        <a class="nav-link dropdown-toggle topbar-icon-link" href="#" role="button" id="notificationsDropdown" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="fas fa-bell"></i>
             <span class="badge bg-danger rounded-pill">{{ Auth::user()->unreadNotifications->count() }}</span>
         </a>
@@ -41,7 +46,10 @@ $empresa = Empresa::first();
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <a class="nav-link dropdown-toggle topbar-profile-link" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="topbar-profile-avatar">{{ strtoupper(substr(auth()->user()->name, 0, 1)) }}</span>
+                <span class="topbar-profile-name d-none d-md-inline">{{ auth()->user()->name }}</span>
+            </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 @can('ver-perfil')
                 <li><a class="dropdown-item" href="{{ route('profile.index') }}">Configuraciones</a></li>
